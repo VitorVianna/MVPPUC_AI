9b3f031f7690c0868d4eccbfba6f298531ef021a
Vitor Henrique Vianna Reis <tetrisit.vreis@terceiros.mag.com.br>
2024-09-22 10:58:56 -0300

    backend api

--- Api/model/__init__.py
+++ Api/model/__init__.py
@@ -4,6 +4,11 @@ from sqlalchemy import create_engine
 import os
 # importando os elementos definidos no modelo
 from model.base import Base
+from model.modelo import Model
+from model.pipeline import Pipeline
+from model.preprocessador import PreProcessador
+from model.avaliador import Avaliador
+from model.carregador import Carregador
 
 
 db_path = "database/"
